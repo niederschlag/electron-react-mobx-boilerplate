fbe944e189397b3d6202395f5133226f790fb88f
niederschlag <hello@krkn.co>
2018-05-17 18:06:18 +0200

    Added possibility to add testing with jest/enzyme

---
+++ __tests__/example.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+describe('example', () => {
+    it('should be able to add 2 and 3', () => {
+        expect(2 + 3).toBe(5);
+    });
+});
